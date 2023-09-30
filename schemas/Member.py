20b6de0d5f4598c9e51047f998314b7d344d6766
gashawgedef@gmail.com <gashawgedef@gmail.com>
2023-09-30 10:21:36 -0700

    members schema models

--- schemas/Member.py
+++ schemas/Member.py
@@ -1,11 +1,15 @@
 
 
-
 from pydantic import BaseModel
-
-
 class Members(BaseModel):
     first_name:str
+    middle_name:str
     last_name:str
     phone:str
     email:str
+    profession:str
+    birth_date:str
+    birth_place_region:str
+    birth_place_zone:str
+    birth_place_wereda:str
+    birth_place_kebele:str
