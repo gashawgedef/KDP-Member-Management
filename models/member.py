ca8b6e6335f5397d768247a370b10c7f5db8a457
gashawgedef@gmail.com <gashawgedef@gmail.com>
2023-09-30 10:33:13 -0700

    members schema models initial create

---
+++ models/member.py
@@ -0,0 +1,23 @@
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column
+
+from ..database import Base
+from sqlalchemy.orm import relationship
+
+
+class Member(Base):
+    __tablename__ = "members"
+    id = Column(Integer, primary_key=True, index=True)
+    first_name: Mapped[str] = mapped_column()
+    middle_name: Mapped[str] = mapped_column()
+    last_name: Mapped[str] = mapped_column()
+    phone: Mapped[str] = mapped_column()
+    email: Mapped[str] = mapped_column()
+    profession: Mapped[str] = mapped_column()
+    birth_date: Mapped[str] = mapped_column()
+    birth_place_region: Mapped[str] = mapped_column()
+    birth_place_zone: Mapped[str] = mapped_column()
+    birth_place_wereda: Mapped[str] = mapped_column()
+    birth_place_kebele:Mapped[str]=mapped_column()
+
+    #user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
